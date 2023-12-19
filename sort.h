be7255290649d1d931d3a716b8d91f4ecdf2dab3
archMk <victormurithi2020@gmail.com>
2023-12-20 01:15:09 +0300

    added quick sort algorithm

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef _SORT_H
 #define _SORT_H
 
+#include <stddef.h>
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdbool.h>
@@ -30,8 +31,10 @@ typedef struct listint_s
 
 /*Builtin header files */
 void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void quick_sort_r(int *A, int start, int end, size_t size);
 #endif
